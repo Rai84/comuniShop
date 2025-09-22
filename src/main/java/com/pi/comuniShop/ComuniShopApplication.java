3d7be8507ef428a643d60e6afc190ad19c356910
Rai <rai.mgoncalves@senacsp.edu.br>
2025-09-22 00:23:33 -0300

    Primeiro commit

---
+++ src/main/java/com/pi/comuniShop/ComuniShopApplication.java
@@ -0,0 +1,13 @@
+package com.pi.comuniShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComuniShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComuniShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pi/comuniShop/ComuniShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.comuniShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComuniShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
